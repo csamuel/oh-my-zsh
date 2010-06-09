222fbeafaefc498a820ed61fa6a2d1a1dea0bacc
Chris Samuel <csamuel@gmail.com>
2010-06-09 13:31:22 -0500

    Adding paths and alisases

---
+++ custom/aliases.zsh
@@ -0,0 +1,108 @@
+# cd
+alias ..='cd ..'
+ 
+# clear
+alias cls=clear
+ 
+# git
+alias gl='git pull'
+alias gp='git push'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gitlog='git log --pretty=oneline --abbrev-commit HEAD~10..HEAD'
+ 
+# rails
+alias sc='script/console'
+alias ss='script/server'
+alias sg='script/generate'
+alias a='autotest -rails'
+alias tlog='tail -f log/development.log'
+alias scaffold='script/generate nifty_scaffold'
+alias migrate='rake db:migrate db:test:clone'
+alias rst='touch tmp/restart.txt'
+ 
+# commands starting with % for pasting from web
+alias %=' '
+
+# misc
+alias reload='. ~/.bash_profile'
+
+# clear
+alias cls='clear'
+
+# servers
+alias dreamhost='ssh csamuel@itcannot.be'
+alias deva01='ssh csamue4916c@pacdcdtadeva01.cable.comcast.com'
+alias deva02='ssh csamue4916c@pacdcdtadeva02.cable.comcast.com'
+alias deva03='ssh csamue4916c@pacdcdtadeva03.cable.comcast.com'
+alias deva04='ssh csamue4916c@pacdcdtadeva04.cable.comcast.com'
+alias stgrpt01='ssh csamue4916c@pasundtastgrpt1.cable.comcast.com'
+alias prdrpt01='ssh csamue4916c@pacdcdtaprdrpt1.cable.comcast.com'
+alias ftp1='ssh csamue4916c@pacdcdtaomprdftp1'
+alias greg='ssh ghenge3254c@pacdcdtaprdrpt1.cable.comcast.com'
+
+
+#vpn
+alias boston='sudo vpnc boston --natt-mode cisco-udp'
+
+#dasadmin
+alias dasadmin='sudo -H -u dasadmin bash -l'
+
+# changing directory to code project
+function c { cd ~/code/$1; }
+
+# alternative to "rails" command to use templates
+function railsapp {
+  template=$1
+  appname=$2
+  shift 2
+  rails $appname -m http://github.com/ryanb/rails-templates/raw/master/$template.rb $@
+}
+
+function take() { 
+    mkdir -p "$1"
+    cd "$1" 
+}
+
+# From http://github.com/suztomo/dotfiles
+function rmf(){
+    for file in $*
+    do
+        __rm_single_file $file
+    done
+}
+
+function __rm_single_file(){
+    if ! [ -d ~/.Trash/ ]
+    then
+        command /bin/mkdir ~/.Trash
+    fi
+
+    if ! [ $# -eq 1 ]
+    then
+        echo "__rm_single_file: 1 argument required but $# passed."
+        exit
+    fi
+
+    if [ -e $1 ]
+    then
+        BASENAME=`basename $1`
+        NAME=$BASENAME
+        COUNT=0
+        while [ -e ~/.Trash/$NAME ]
+        do
+            COUNT=$(($COUNT+1))
+            NAME="$BASENAME.$COUNT"
+        done
+
+        command /bin/mv $1 ~/.Trash/$NAME
+    else
+        echo "No such file or directory: $file"
+    fi
+}
+
---
+++ custom/paths.zsh
@@ -0,0 +1,47 @@
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export EC2_PRIVATE_KEY=$HOME/.ec2/pk-KWX5OM554TXYEMORMTTI6C22ONFQEG4T.pem
+export EC2_CERT=$HOME/.ec2/cert-KWX5OM554TXYEMORMTTI6C22ONFQEG4T.pem
+export EC2_HOME=$HOME/Library/ec2-api-tools-1.3-30349/
+
+# Ruby
+export LOCAL_GEM_PATH=$HOME/.gem/ruby/1.8/bin
+
+#export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
+export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.5.0/Home
+export CATALINA_HOME=/opt/Tomcat/Home
+
+export JAXWS_HOME=/opt/local/share/java/jaxws-ri/
+
+# Grails and Groovy
+export GRAILS_HOME="/opt/local/share/java/grails"
+export GROOVY_HOME="/opt/local/share/java/groovy"
+
+# Oracle Instant Client
+export DYLD_LIBRARY_PATH="/usr/local/oracle/instantclient_10_2:$DYLD_LIBRARY_PATH"
+export SQLPATH=$DYLD_LIBRARY_PATH
+export TNS_ADMIN="/usr/local/oracle/network/admin"
+export NLS_LANG="AMERICAN_AMERICA.UTF8"
+export RC_ARCHS=i386
+
+# Grep and History
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='7;33'
+export GIT_EDITOR=mate
+export HISTCONTROL=erasedups
+export HISTSIZE=5000
+
+# -- start rip config -- #
+RIPDIR=/Users/csamuel/.rip
+RUBYLIB="$RUBYLIB:$RIPDIR/active/lib"
+PATH="$PATH:$RIPDIR/active/bin"
+export RIPDIR RUBYLIB PATH
+# -- end rip config -- #
+
+
+export AXIS2_HOME=$HOME/code/axis2
+
+export M2_HOME="/opt/local/share/java/maven2"
+
+# Path
+export PATH=$HOME/.local/homebrew/bin:/opt/local/bin:/opt/local/sbin:$EC2_HOME/bin:LOCAL_GEM_PATH:/opt/flex_sdk_3/bin:$GROOVY_HOME/bin:$CATALINA_HOME/bin:$HOME/opt/local/bin:/opt/CollabNet_Subversion/bin:$HOME/.gem/ruby/1.8/bin:$HOME/.javagem/java/bin:$HOME/bin:$PATH
