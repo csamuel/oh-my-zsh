dbf407120be89efa0920381d33c230872e5b7e87
Chris Samuel <csamuel@gmail.com>
2011-05-09 14:10:43 -0500

    Various env cleanup

--- custom/paths.zsh
+++ custom/paths.zsh
@@ -1,5 +1,3 @@
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
 # JRebel
 export REBEL_HOME="/Applications/ZeroTurnaround/JRebel"
 
@@ -7,8 +5,8 @@ export REBEL_HOME="/Applications/ZeroTurnaround/JRebel"
 export JAVA_HOME=$(/usr/libexec/java_home)
 export JAVA_OPTS="-Xmx512M -XX:MaxPermSize=128M -client"
 
-# Maven
-export M2_HOME="/usr/local/Cellar/maven/3.0/libexec"
+# Unset M2_HOME because it causes problems with Maven 3
+unset M2_HOME
 
 # Mirah
 export MIRAH_HOME="$HOME/projects/mirah"
@@ -28,10 +26,13 @@ export HISTSIZE=5000
 # Torquebox
 export TORQUEBOX_HOME="/usr/local/torquebox-1.0.0"
 export JBOSS_HOME="$TORQUEBOX_HOME/jboss"
+export JRUBY_HOME="/usr/local/Cellar/jruby/1.6.1/jruby"
 
 # Git (https://github.com/b4winckler/macvim/wiki/FAQ)
 export GIT_EDITOR='mvim -f --nomru -c "au VimLeave * !open -a Terminal"'
 
+# Man Entries
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
 # Path
-export PATH="$PATH:$HOME/.bin:/usr/local/bin:/usr/local/sbin:$HOME/.rvm/bin:/usr/local/Cellar/python/2.7.1/bin"
-export JRUBY_HOME=/Users/csamuel/.rvm/rubies/jruby-1.6.1
+export PATH="$PATH:$HOME/.bin:/usr/local/bin:/usr/local/sbin:$HOME/.rvm/bin"
