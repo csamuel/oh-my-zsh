492545228b64cf0ad3b5b593365f53d330be24c4
Chris Samuel <csamuel@gmail.com>
2011-08-18 16:33:51 -0500

    Add some more JVM options to play with

--- custom/paths.zsh
+++ custom/paths.zsh
@@ -3,8 +3,14 @@ export REBEL_HOME="/Applications/ZeroTurnaround/JRebel"
 
 # OS X Java Preference setting
 # export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
+# export JAVA_HOME="$HOME/Library/Java/JavaVirtualMachines/1.7.0.jdk/Contents/Home"
 export JAVA_HOME=$(/usr/libexec/java_home)
-export JAVA_OPTS="-Xmx512M -XX:MaxPermSize=128M"
+export JAVA_OPTS="-Xmx512M -XX:MaxPermSize=128M -d32 -client"
+# export JAVA_OPTS="-Xmx512M -XX:MaxPermSize=128M -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4000,suspend=n"
+
+
+# export JRUBY_OPTS="-J-d32 -J-client"
+# export JRUBY_OPTS="-J-d32 -J-client -J-XX:CompileCommand=dontinline,org.jruby.runtime.invokedynamic.InvokeDynamicSupport::invocationFallback"
 
 # Unset M2_HOME because it causes problems with Maven 3
 unset M2_HOME
@@ -27,7 +33,7 @@ export HISTSIZE=5000
 # Torquebox
 export TORQUEBOX_HOME="/usr/local/torquebox-1.0.0"
 export JBOSS_HOME="$TORQUEBOX_HOME/jboss"
-export JRUBY_HOME="/usr/local/Cellar/jruby/1.6.2/jruby"
+# export JRUBY_HOME="/usr/local/Cellar/jruby/1.6.2/jruby"
 
 # Git (https://github.com/b4winckler/macvim/wiki/FAQ)
 export GIT_EDITOR='mvim -f --nomru -c "au VimLeave * !open -a iTerm\ 2"'
@@ -36,4 +42,4 @@ export GIT_EDITOR='mvim -f --nomru -c "au VimLeave * !open -a iTerm\ 2"'
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 # Path
-export PATH="$PATH:$HOME/.bin:/usr/local/bin:/usr/local/sbin:$HOME/.rvm/bin"
+export PATH="$PATH:$HOME/.bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$HOME/.rvm/bin"
